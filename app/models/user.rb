feefe7a8b05ee48aacc7fa740d4aa37c3cd4db1f
Iyayi Roland <iyayiroland@gmail.com>
2023-08-28 16:08:20 +0200

    Added association and validations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+  has_many :events
+  has_many :posts
+  has_many :comments
+  has_many :likes, as: :likable
+  has_many :tasks, through: :user_tasks
+  has_many :user_tasks
+  has_many :attendees
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
