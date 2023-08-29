c14f4374d4fea60d09871383b3123e2b66baceea
Farah Boughalem <boughalemfarah@gmail.com>
2023-08-29 16:42:40 +0200

    rainbow border

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
-<<<<<<< HEAD
-=======
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
->>>>>>> 4098f734acedcacf0ed11b58966a021e71f8e8ef
 
 end
