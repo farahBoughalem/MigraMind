31158818a6566565b74ae79d9661c974cf76b48f
Olivia Feldman <orwfeldman@gmail.com>
2023-08-30 17:56:12 +0200

    Added user task controller, etc. and SCSS to Tasks page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,14 +3,22 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def update
-    if @task.update(task_params)
-    respond_to do |format|
-      format.html { redirect_to tasks_path, notice: 'Task is completed.' }
-      format.js
-    end
-    else
-    #something something here
-    end
-  end
+  # def update
+  #   raise
+  #   if @task.update(task_params)
+
+  #   respond_to do |format|
+  #     format.html { redirect_to tasks_path, notice: 'Task is completed.' }
+  #     format.js
+  #   end
+  #   else
+  #   #something something here
+  #   end
+  # end
+
+  # private
+
+  # def task_params
+  #   params.require(:task).permit(:status)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  get 'user_tasks/controller'
   devise_for :users, controllers: { registrations: 'registrations' }
 
   root to: "posts#index"
 
   resources :events
-  resources :tasks
-  resources :tasks, only: :index
+  resources :tasks do
+    resources :user_tasks, only: [:index, :update]
+  end
+
+  resources :user_tasks, only: :index
+
   resources :posts, only: :index
 
 
