8eda716db10f550e4d0dfcaf72fc3fe497f25d55
Olivia Feldman <orwfeldman@gmail.com>
2023-08-30 15:43:15 +0200

    Updated Application Controller, created user tasks

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < Devise::RegistrationsController
     Task.all.each do |task|
       user_task = UserTask.new
       user_task.task = task
-      user_task.user = self
+      user_task.user = current_user
       user_task.status = 0
       user_task.save
     end
