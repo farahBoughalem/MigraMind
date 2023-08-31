8a229d36f9ca02fb9588d28dc1c5aeeaf89aa5fc
Iyayi Roland <iyayiroland@gmail.com>
2023-08-31 12:37:51 +0200

    geocode

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,7 @@ class Event < ApplicationRecord
   validates :date, presence: true
 
   has_one_attached :photo
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
