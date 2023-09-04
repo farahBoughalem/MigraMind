6f308aaf10fac987ab8c99bac49e584abf1de729
Farah Boughalem <boughalemfarah@gmail.com>
2023-09-04 17:23:23 +0200

    like/dislike

--- app/javascript/controllers/like_button_controller.js
+++ app/javascript/controllers/like_button_controller.js
@@ -3,32 +3,39 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="like-button"
 export default class extends Controller {
   static targets = ["sum", "container"]
+
   connect() {
-    // console.log(this.buttonTarget)
+    // console.log(this.likedValue)
   }
 
-  like(event){
-    event.preventDefault()
-    // console.log("LIKE<3")
-    const postId = event.currentTarget.dataset.likeButtonPostId;
-    const likableType = event.currentTarget.dataset.likeButtonLikableType;
-    // console.log(event.currentTarget.dataset.likeButtonLikableType)
-    if (likableType === "post") {
-      fetch(`/posts/${postId}/likes`,{
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content
-        },
-        body: JSON.stringify({})
-      })
-        .then(response => response.json())
-        .then(data => {
-          // console.log("Like created successfully:", data);
-          this.sumTarget.innerHTML = parseInt(this.sumTarget.innerHTML) + 1
-          this.containerTarget.classList.remove("blacky")
-          this.containerTarget.classList.add("greeny")
-        })
+  like(event) {
+  event.preventDefault()
+  const postId = event.currentTarget.dataset.likeButtonPostId;
+  const likableType = event.currentTarget.dataset.likeButtonLikableType;
+  // console.log(event.currentTarget.dataset.likeButtonLikableType)
+  if (likableType === "post") {
+    fetch(`/posts/${postId}/likes`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content
+      },
+      body: JSON.stringify({})
+    })
+    .then(response => response.json())
+    .then(data => {
+      // console.log("Like created successfully:", data);
+      if (data.value === "deleted") {
+        this.sumTarget.innerHTML = parseInt(this.sumTarget.innerHTML) - 1
+        this.containerTarget.classList.add("blacky")
+        this.containerTarget.classList.remove("greeny")
+
+      } else {
+        this.sumTarget.innerHTML = parseInt(this.sumTarget.innerHTML) + 1
+        this.containerTarget.classList.remove("blacky")
+        this.containerTarget.classList.add("greeny")
+      }
+    })
     }else{
       const commentId = event.currentTarget.dataset.likeButtonCommentId;
       // console.log(commentId)
@@ -43,11 +50,16 @@ export default class extends Controller {
       .then(response => response.json())
       .then(data => {
         // console.log("Like created successfully:", data);
+      if (data.value === "deleted") {
+        this.sumTarget.innerHTML = parseInt(this.sumTarget.innerHTML) - 1
+        this.containerTarget.classList.add("blacky")
+        this.containerTarget.classList.remove("greeny")
+
+      } else {
         this.sumTarget.innerHTML = parseInt(this.sumTarget.innerHTML) + 1
         this.containerTarget.classList.remove("blacky")
         this.containerTarget.classList.add("greeny")
-      })
-    }
-
+      }
+    })
   }
-}
+}}
