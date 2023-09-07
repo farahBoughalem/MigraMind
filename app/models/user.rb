a208300d2da6dae27334b1ba7c573d8aef0bf785
Farah Boughalem <boughalemfarah@gmail.com>
2023-09-07 11:24:21 +0200

    associate

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
   has_many :chatrooms_as_sender, class_name: "Chatroom", foreign_key: :sender_id
   has_many :chatrooms_as_receiver, class_name: "Chatroom", foreign_key: :receiver_id
   has_many :messages, dependent: :destroy
+  has_many :chatrooms, dependent: :destroy
 
   has_one_attached :photo
   # Include default devise modules. Others available are:
