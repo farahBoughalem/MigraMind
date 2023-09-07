12541bac420d0bc28ec56c651cf9746fbad93ebd
Farah Boughalem <boughalemfarah@gmail.com>
2023-09-07 16:54:24 +0200

    empty comment

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,14 +4,16 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
-    if @message.save
-      ChatroomChannel.broadcast_to(
-        @chatroom,
-        render_to_string(partial: "message", locals: {message: @message})
-      )
-      head :ok
-    else
-      render "chatrooms/show", status: :unprocessable_entity
+    if @message.content.present?
+      if @message.save
+        ChatroomChannel.broadcast_to(
+          @chatroom,
+          render_to_string(partial: "message", locals: {message: @message})
+        )
+        head :ok
+      else
+        render "chatrooms/show", status: :unprocessable_entity
+      end
     end
   end
 
